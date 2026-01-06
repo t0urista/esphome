f9052aedbd29704c73bc2e1a2d6af76b786087dd
t0urista <damien.collart@hotmail.be>
2026-01-06 14:07:57 +0100

    Modify CRC check handling in teleinfo.cpp
    Commented out CRC check and added error logging for bad CRC.

--- esphome/components/teleinfo/teleinfo.cpp
+++ esphome/components/teleinfo/teleinfo.cpp
@@ -133,17 +133,23 @@ void TeleInfo::loop() {
           ESP_LOGE(TAG, "No group found");
           break;
         }
-
-        if (!check_crc_(buf_finger, grp_end))
-          continue;
-
+/* DCO 
+         if (!check_crc_(buf_finger, grp_end))
+            continue;
+*/
         /* Get tag */
         field_len = get_field(tag_, buf_finger, grp_end, separator_, MAX_TAG_SIZE);
         if (!field_len || field_len >= MAX_TAG_SIZE) {
           ESP_LOGE(TAG, "Invalid tag.");
           continue;
         }
+        
+/* DCO  */
+         if (!check_crc_(buf_finger, grp_end)) {
+             ESP_LOGE(TAG, "bad crc with tag. %", tag_);
+         }
 
+/* DCP */
         /* Advance buf_finger to after the tag and the separator. */
         buf_finger += field_len + 1;
 
