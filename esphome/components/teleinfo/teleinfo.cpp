79782c9ed23d71134369742da4c8a95f946862dc
t0urista <damien.collart@hotmail.be>
2026-01-06 18:03:55 +0100

    Fix log message to indicate expected CRC value

--- esphome/components/teleinfo/teleinfo.cpp
+++ esphome/components/teleinfo/teleinfo.cpp
@@ -35,7 +35,7 @@ bool TeleInfo::check_crc_(const char *grp, const char *grp_end) {
   crc_tmp &= 0x3F;
   crc_tmp += 0x20;
   if (raw_crc != crc_tmp) {
-    ESP_LOGE(TAG, "bad crc: got %d except %d", raw_crc, crc_tmp);
+    ESP_LOGE(TAG, "bad crc: got %d expected %d", raw_crc, crc_tmp);
     return false;
   }
 
@@ -147,6 +147,8 @@ void TeleInfo::loop() {
 /* DCO  */
          if (!check_crc_(buf_finger, grp_end)) {
              ESP_LOGE(TAG, "bad crc with tag. %s", tag_);
+         }else {
+             ESP_LOGE(TAG, "good crc with tag. %s", tag_);
          }
 
 /* DCP */
